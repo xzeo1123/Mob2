0cce962ddae24043299d3e420ca455df6e7b5bf3
xzeo1123 <xzeo1123@gmail.com>
2024-03-18 09:54:50 +0700

    i don't know

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btnStarted);
+        btn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/androidproject/entity/Account.java
@@ -0,0 +1,90 @@
+package com.example.androidproject.entity;
+
+public class Account {
+    private long accountID;
+    private String email;
+    private String password;
+    private float money;
+    private int confirmedCode;
+    private String fullName;
+    private String dob;
+    private boolean isVip;
+
+    public Account() {
+    }
+
+    public Account(long accountID, String email, String password, float money, int confirmedCode, String fullName, String dob, boolean isVip){
+        this.accountID = accountID;
+        this.email = email;
+        this.password = password;
+        this.money = money;
+        this.confirmedCode = confirmedCode;
+        this.fullName = fullName;
+        this.dob = dob;
+        this.isVip = isVip;
+    }
+
+    public long getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public float getMoney() {
+        return money;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
+
+    public int getConfirmedCode() {
+        return confirmedCode;
+    }
+
+    public void setConfirmedCode(int confirmedCode) {
+        this.confirmedCode = confirmedCode;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public boolean getVip() {
+        return isVip;
+    }
+
+    public void setVip(boolean vip) {
+        isVip = vip;
+    }
+}
