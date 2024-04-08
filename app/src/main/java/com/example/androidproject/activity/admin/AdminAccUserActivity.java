a09ad511c804f446b65b75d11bf8db0fb0cec9f2
A1erice <128226905+A1erice@users.noreply.github.com>
2024-04-08 21:21:45 +0700

    add basic
    DAO, entity, dtb
    Restructor files

--- app/src/main/java/com/example/androidproject/activity/admin/AdminAccUserActivity.java
+++ app/src/main/java/com/example/androidproject/activity/admin/AdminAccUserActivity.java
@@ -1,4 +1,4 @@
-package com.example.androidproject;
+package com.example.androidproject.activity.admin;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class UserHomeActivity extends AppCompatActivity {
+import com.example.androidproject.R;
+
+public class AdminAccUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user_home);
+        setContentView(R.layout.activity_admin_acc_user);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/androidproject/activity/bookcase/BCListActivity.java
+++ app/src/main/java/com/example/androidproject/activity/bookcase/BCListActivity.java
@@ -1,4 +1,4 @@
-package com.example.androidproject;
+package com.example.androidproject.activity.bookcase;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AdminNewMgaActivity extends AppCompatActivity {
+import com.example.androidproject.R;
+
+public class BCListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_new_mga);
+        setContentView(R.layout.activity_bclist);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
