751cf271315008c94f854cecc6bddb06c197a466
A1erice <128226905+A1erice@users.noreply.github.com>
2024-04-09 09:02:22 +0700

    Revert "add basic"
    This reverts commit a09ad511c804f446b65b75d11bf8db0fb0cec9f2.

--- app/src/main/java/com/example/androidproject/BCFavorActivity.java
+++ app/src/main/java/com/example/androidproject/BCFavorActivity.java
@@ -1,4 +1,4 @@
-package com.example.androidproject.activity.bookcase;
+package com.example.androidproject;
 
 import android.os.Bundle;
 
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.example.androidproject.R;
 
 public class BCFavorActivity extends AppCompatActivity {
 
